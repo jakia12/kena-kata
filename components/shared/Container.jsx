d151c35ba5a8c0d883228b1bb5365c830633db80
jakia911 <gazijakia911@gmail.com>
2025-12-27 21:56:10 +0600

    fix:some syntax error solve for container shared component

--- components/shared/Container.jsx
+++ components/shared/Container.jsx
@@ -1,12 +1,6 @@
 import { cn } from "@/lib/utils";
 
-export function Container({
-  className,
-  children,
-}: {
-  className?: string,
-  children: React.ReactNode,
-}) {
+export function Container({ className, children }) {
   return (
     <div className={cn("mx-auto w-full max-w-7xl px-4 sm:px-6", className)}>
       {children}
